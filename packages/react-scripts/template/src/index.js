8f307aef17a8353b6ce1f71173d5342e2d97b554
michael <mihai@mandrescu.co>
2017-06-25 22:49:25 +0100

    Test

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import './styles/index.css';
+import App from './main_app/App';
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
